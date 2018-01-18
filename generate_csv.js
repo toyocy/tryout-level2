b422ade98e95e14c5ca1ae95d9dd4e73b185efa6
Takayuki Toyoshima <tktyo1043@yahoo.co.jp>
2018-01-18 22:44:50 +0900

    Implement get data for export

--- generate_csv.js
+++ generate_csv.js
@@ -1,7 +1,7 @@
 (function() {
   "use strict";
-  kintone.events.on('app.record.index.show', function(event){
-    if (event.viewName !== '出力用一覧（当月&出力済除外）'){
+  kintone.events.on('app.record.index.show', function(events){
+    if (events.viewName !== '出力用一覧（当月&出力済除外）'){
       return;
     }
 
@@ -9,8 +9,29 @@
     myIndexButton.id = 'my_index_buton';
     myIndexButton.innerHTML = 'CSV出力'
 
+    var csv = [];
+    for (var i = 0; i < events.records.length; i++){
+      var record = events.records[i];
+      var value = [];
+      if (record.is_checked_by_tax_accountant.value != "済") { continue; }
+      value.push(record.export_to_csv.value);
+      value.push(record.is_checked_by_tax_accountant.value);
+      value.push(record.tax.value);
+      value.push(record.payer_name.value);
+      value.push(record.expense.value);
+      value.push(record.content.value);
+      value.push(record.amount_of_money.value);
+      value.push(record.recipt.value);
+      value.push(record['作成日時'].value);
+      csv.push(value);
+    }
+
     myIndexButton.onclick = function() {
-      window.confirm('CSVを出力します');
+      var confirmResponse = confirm('CSVを出力します');
+      if (confirmResponse) {
+        console.log(csv);
+      }
+      return;
     };
 
     kintone.app.getHeaderMenuSpaceElement().appendChild(myIndexButton);
