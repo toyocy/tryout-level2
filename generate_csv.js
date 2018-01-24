44252b31529fa6e161cd99063d43d90bdbaea29a
Takayuki Toyoshima <tktyo1043@yahoo.co.jp>
2018-01-24 22:57:32 +0900

    finished export all records

--- generate_csv.js
+++ generate_csv.js
@@ -1,69 +1,35 @@
 (function() {
+
   "use strict";
-  kintone.events.on('app.record.index.show', function(event){
-    if (event.viewName !== '出力用一覧（当月&出力済除外）'){
-      return;
-    }
 
-    if (document.getElementById('export-csv') !== null) {
-      return;
-    }
-
-    var exportButton = document.createElement('button');
-    exportButton.id = 'export-csv';
-    exportButton.innerHTML = 'CSV出力'
-
-    var escapeString = function(value) {
-      return '"' + (value? value.replace(/"/g, '""'): '') + '"';
-    };
-        
     if ((window.URL || window.webkitURL).createObjectURL == null) {
-      // サポートされていないブラウザ
+      // non supported browser
       return;
     }
   
-    var csv = [];
-    var row = [];
-    for (var i = 0; i < event.records.length; i++){
-      var record = event.records[i];
-      row = [];
-      if (record.is_checked_by_tax_accountant.value != "済") { continue; }
-      row.push(record.export_to_csv.value);
-      row.push(record.is_checked_by_tax_accountant.value);
-      row.push(record.tax.value);
-      row.push(record.payer_name.value);
-      row.push(record.expense.value);
-      row.push(record.content.value);
-      row.push(record.amount_of_money.value);
-      row.push(record.recipt.value);
-      row.push(record['作成日時'].value);
-      csv.push(row);
-    }
+    function downloadCSV(expenseCsvData) {
+      var stringToArray = function(csv) {
+        var array = [],i,il = csv.length;
+        for (i=0; i<il; i++) array.push(csv.charCodeAt(i));
+        return array;
+      };
+  
+      var csvbuf = expenseCsvData.map(function(e){return e.join(',')}).join('\r\n');
+      var array = stringToArray(csvbuf);
+      var sjisArray = Encoding.convert(array, "SJIS", "UNICODE");
+      var uint8Array = new Uint8Array(sjisArray);
+      var blob = new Blob([uint8Array], { type: 'text/csv' });
+      
+      var url = (window.URL || window.webkitURL).createObjectURL(blob);
+      var fileName = "export.csv";
 
-    var stringToArray = function(string) {
-      var array = [],i,il=string.length;
-      for (i=0; i<il; i++) array.push(string.charCodeAt(i));
-      return array;
-    };
-
-    var csvbuf = csv.map(function(e){return e.join(',')}).join('\r\n');
-
-    var array = stringToArray(csvbuf);
-    var sjisArray = Encoding.convert(array, "SJIS", "UNICODE");
-    var uint8Array = new Uint8Array(sjisArray);
-    var blob = new Blob([uint8Array], { type: 'text/csv' });
-    
-    var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    var fileName = "export.csv"
-
-    exportButton.onclick = function() {
       var confirmResponse = confirm('CSVを出力します');
       if (confirmResponse) {
         if (window.navigator.msSaveOrOpenBlob) {
           // for IE
           window.navigator.msSaveOrOpenBlob(blob, fileName);
         } else {
-          //for not IE
+          // for not IE
           var link = document.createElement('a');
           var e = document.createEvent('MouseEvents');
           e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
@@ -71,10 +37,69 @@
           link.href = url;
           link.dispatchEvent(e);
         }
+
+        // ここにCSV出力のチェックを入れる
+
       }else{
         return;
       }
-    };
-    kintone.app.getHeaderMenuSpaceElement().appendChild(exportButton);
+    }
+
+    function fetchRecords(appId, offset, limit, records) {
+      var offset = offset || 0;
+      var limit = limit || 100;
+      var allRecords = records || [];
+      var params = { app: appId, query: 'order by レコード番号 asc limit ' + limit + ' offset ' + offset };
+      return kintone.api('/k/v1/records', 'GET', params).then(function(res) {
+        allRecords = allRecords.concat(res.records);
+        if (res.records.length === limit ) {
+          return fetchRecords(appId, offset + limit, limit, allRecords);
+        }
+        return allRecords;
+      });
+    }
+
+    function createCsvFile(records) {
+      var expenseCsvData = [];
+      var row = [];
+      for (var j = 0; j < records.length; j++){
+        var record = records[j];
+        row = [];
+        if (record.is_checked_by_tax_accountant.value != "済") { continue; }
+        row.push(record.export_to_csv.value);
+        row.push(record.is_checked_by_tax_accountant.value);
+        row.push(record.tax.value);
+        row.push(record.payer_name.value);
+        row.push(record.expense.value);
+        row.push(record.content.value);
+        row.push(record.amount_of_money.value);
+        row.push(record.recipt.value);
+        row.push(record['作成日時'].value);
+        expenseCsvData.push(row);
+      }
+      downloadCSV(expenseCsvData);
+    }
+    
+    kintone.events.on('app.record.index.show', function(event){
+      if (event.viewName !== '出力用一覧（当月&出力済除外）'){
+        return;
+      }
+  
+      if (document.getElementById('export-csv') !== null) {
+        return;
+      }
+
+      var headerElement = kintone.app.getHeaderMenuSpaceElement();
+      var exportButton = document.createElement('button');
+      exportButton.id = 'export-csv';
+      exportButton.innerHTML = 'CSV出力';
+
+      exportButton.onclick = function() {
+        fetchRecords(kintone.app.getId()).then(function(records) {
+          createCsvFile(records);
+        });
+      };
+
+    headerElement.appendChild(exportButton);
   });
 })();
